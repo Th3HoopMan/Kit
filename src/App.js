f80b8a95caa5f27afca6b24f85ef1358c70263d2
Joseph Hooper <stayvaliant94@gmail.com>
2020-08-02 23:43:32 -0700

    Add conditional rendering based on if kits exists
    If there are no kits app will show an empty message. Form will be
    visible when user clicks create kit.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 import KitForm from './KitForm'
 import KitList from './KitList';
-import { KitProvider } from './KitContext';
+import { KitProvider, KitContext } from './KitContext';
 import Kit from './Kit';
 
 function App() {
-  return (
-    <KitProvider>
-      <div className="App">
+  const [isFormVisible, setIsFormVisible] = useState(false);
+  const [kits, setKits] = useContext(KitContext);
 
-        {/* If no kits have been create show empty message */}
+  const showKitForm = () => {
 
-        {/* Else Default State */}
-        <KitForm/>
-        <KitList/>
+  };
+
+  if (kits.length == 0 && !isFormVisible) {
+    return(
+      <div>
+        <h1>
+          You don't currently have any Kits!
+        </h1>
+        <br/>
+        <button onClick={() => setIsFormVisible(true)}>Create Kit</button>
       </div>
-    </KitProvider>
-  );
+    );
+  } else {
+    return (
+      <KitProvider>
+        <div className="App">
+          <KitForm/>
+          <KitList/>
+        </div>
+      </KitProvider>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { KitProvider } from './KitContext';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <KitProvider>
+      <App />
+    </KitProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
